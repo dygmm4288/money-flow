c20a2d4f5c3f76cf1fd049f9e23bb3f6d122565f
dygmm4288 <55860930+dygmm4288@users.noreply.github.com>
2025-01-12 22:29:42 +0900

    (update) pay_total

--- src/app/api/pay_total/route.ts
+++ src/app/api/pay_total/route.ts
@@ -1,18 +1,28 @@
-import { type EmailOtpType } from "@supabase/supabase-js";
-import { type NextRequest } from "next/server";
+import { createClient } from "@/lib/supabase/server/server";
+import { NextResponse } from "next/server";
 
-import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
+export type TotalAmount = {
+  category: string;
+  total_amount: number;
+};
 
-export async function GET(request: NextRequest) {
-    const supabase = await createClient();
+export async function GET() {
+  const supabase = await createClient();
 
-    const { data: expanse } = await supabase.rpc('get_exense_total');
-    const { data: income } = await supabase.rpc('get_income_total');
+  const { data: expense, error: expenseError } = await supabase.rpc(
+    "get_expense_total",
+  );
 
+  const { data: income, error: incomeError } = await supabase.rpc(
+    "get_income_total",
+  );
 
-    return {
-        expanse,
-        income
-    };
+  if (expenseError || incomeError) {
+    return NextResponse.json(
+      { message: "Something went wrong", expenseError, incomeError },
+      { status: 500 },
+    );
+  }
+
+  return NextResponse.json({ expense, income }, { status: 200 });
 }
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,20 @@
 import { get } from "@/lib/supabase/server/get";
-import _ from "lodash";
-import PostBtn from "./PostBtn";
-import PutBtn from "./PutBtn";
+import { TotalAmount } from "../api/pay_total/route";
 
 export default async function TestPage() {
   const data = await get("pay");
+  const response = await fetch("http://localhost:3000/api/pay_total", {
+    method: "GET",
+  });
+
+  const total = (await response.json()) as {
+    expense: TotalAmount[];
+    income: TotalAmount[];
+  };
 
   return (
     <div>
-      {_.map(data, (value) =>
+      {/* {_.map(data, (value) =>
         _.map(value, (value, key) => (
           <div>
             <p>key : {key}</p>
@@ -17,7 +23,27 @@ export default async function TestPage() {
         )),
       )}
       <PostBtn />
-      <PutBtn />
+      <PutBtn /> */}
+
+      <h1>expense</h1>
+      <ul>
+        {total.expense.map((item) => (
+          <>
+            <p>{item.category}</p>
+            <p>{item.total_amount}</p>
+          </>
+        ))}
+      </ul>
+
+      <h1>income</h1>
+      <ul>
+        {total.income.map((item) => (
+          <>
+            <p>{item.category}</p>
+            <p>{item.total_amount}</p>
+          </>
+        ))}
+      </ul>
     </div>
   );
 }
